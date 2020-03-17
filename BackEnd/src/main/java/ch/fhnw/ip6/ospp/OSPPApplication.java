d5327172ca83860ee403598735d2df41d91b4458
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-03-17 15:57:38 +0100

    Dockerfile for SpringBoot

--- BackEnd/src/main/java/ch/fhnw/ip6/ospp/OSPPApplication.java
+++ BackEnd/src/main/java/ch/fhnw/ip6/ospp/OSPPApplication.java
@@ -2,16 +2,9 @@ package ch.fhnw.ip6.ospp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OSPPApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(OSPPApplication.class);
-    }
+public class OSPPApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OSPPApplication.class, args);
