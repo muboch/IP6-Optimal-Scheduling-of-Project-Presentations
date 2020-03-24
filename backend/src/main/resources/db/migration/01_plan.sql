efc73d38e0965bda7392047f2daebd1a4610bd3e
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-03-24 22:45:26 +0100

    remove unneeded files

--- backend/src/main/resources/db/migration/01_plan.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists plan
-(
-    id      bigint       not null,
-    created datetime(6)  null,
-    plan_nr varchar(255) null,
-    primary key (id)
-);
--- backend/src/main/resources/db/migration/05_timeslot.sql
+++
@@ -1,8 +0,0 @@
-
-
-create table if not exists timeslot
-(
-    id    bigint      not null,
-    start datetime(6) null,
-    primary key (id)
-);
