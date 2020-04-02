6e81c984943a9ddd4b5a09ab333fe12455c20e5e
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-04-02 23:06:45 +0200

    nativeutils

--- solvers/cpsolver/src/main/java/ch/fhnw/ip6/cpsolver/Program.java
+++ solvers/cpsolver/src/main/java/ch/fhnw/ip6/cpsolver/Program.java
@@ -1,14 +1,24 @@
 package ch.fhnw.ip6.cpsolver;
 
 import ch.fhnw.ip6.api.SolverContext;
+import ch.fhnw.ip6.common.util.NativeUtils;
+
+import java.io.IOException;
 
 public class Program {
-
-    static {
-        System.loadLibrary("jniortools");
+    {
+        try {
+            NativeUtils.loadLibraryFromJar("/resources/lib/libjniortools.so");
+            NativeUtils.loadLibraryFromJar("/resources/lib/libortools.so");
+        } catch (IOException e) {
+            // This is probably not the best way to handle exception :-)
+            e.printStackTrace();
+        }
     }
+
     public static void main(String[] args) {
         Solver solver = new Solver(new SolverContext());
         solver.testSolve();
+
     }
 }
