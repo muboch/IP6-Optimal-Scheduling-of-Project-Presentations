211046c9cde7f803444ac6fd85397daaf160e839
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-04-04 23:48:46 +0200

    project cleanup

---
+++ ospp-application/target/generated-sources/annotations/ch/fhnw/ip6/ospp/mapper/LecturerMapperImpl.java
@@ -0,0 +1,49 @@
+package ch.fhnw.ip6.ospp.mapper;
+
+import ch.fhnw.ip6.common.dto.Lecturer;
+import ch.fhnw.ip6.ospp.vo.LecturerVO;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-04-04T17:13:18+0200",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 13.0.2 (N/A)"
+)
+@Component
+public class LecturerMapperImpl implements LecturerMapper {
+
+    @Override
+    public Lecturer toDto(ch.fhnw.ip6.ospp.model.Lecturer lecturer) {
+        if ( lecturer == null ) {
+            return null;
+        }
+
+        Lecturer lecturer1 = new Lecturer();
+
+        lecturer1.setId( lecturer.getExternalId() );
+        lecturer1.setFirstname( lecturer.getFirstname() );
+        lecturer1.setLastname( lecturer.getLastname() );
+        lecturer1.setEmail( lecturer.getEmail() );
+        lecturer1.setInitials( lecturer.getInitials() );
+
+        return lecturer1;
+    }
+
+    @Override
+    public Lecturer toDto(LecturerVO lecturer) {
+        if ( lecturer == null ) {
+            return null;
+        }
+
+        Lecturer lecturer1 = new Lecturer();
+
+        lecturer1.setId( lecturer.getExternalId() );
+        lecturer1.setFirstname( lecturer.getFirstname() );
+        lecturer1.setLastname( lecturer.getLastname() );
+        lecturer1.setEmail( lecturer.getEmail() );
+        lecturer1.setInitials( lecturer.getInitials() );
+
+        return lecturer1;
+    }
+}
---
+++ ospp-application/target/generated-sources/annotations/ch/fhnw/ip6/ospp/mapper/PlanningMapperImpl.java
@@ -0,0 +1,62 @@
+package ch.fhnw.ip6.ospp.mapper;
+
+import ch.fhnw.ip6.common.dto.Planning;
+import ch.fhnw.ip6.ospp.model.Planning.PlanningBuilder;
+import ch.fhnw.ip6.ospp.vo.PlanningVO;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-04-04T17:13:18+0200",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 13.0.2 (N/A)"
+)
+@Component
+public class PlanningMapperImpl implements PlanningMapper {
+
+    @Override
+    public Planning toDto(ch.fhnw.ip6.ospp.model.Planning planning) {
+        if ( planning == null ) {
+            return null;
+        }
+
+        Planning planning1 = new Planning();
+
+        if ( planning.getNr() != null ) {
+            planning1.setNr( Integer.parseInt( planning.getNr() ) );
+        }
+        planning1.setStatus( planning.getStatus() );
+
+        return planning1;
+    }
+
+    @Override
+    public Planning toDto(PlanningVO planning) {
+        if ( planning == null ) {
+            return null;
+        }
+
+        Planning planning1 = new Planning();
+
+        if ( planning.getNr() != null ) {
+            planning1.setNr( Integer.parseInt( planning.getNr() ) );
+        }
+        planning1.setStatus( planning.getStatus() );
+
+        return planning1;
+    }
+
+    @Override
+    public ch.fhnw.ip6.ospp.model.Planning toEntity(Planning planning) {
+        if ( planning == null ) {
+            return null;
+        }
+
+        PlanningBuilder planning1 = ch.fhnw.ip6.ospp.model.Planning.builder();
+
+        planning1.nr( String.valueOf( planning.getNr() ) );
+        planning1.status( planning.getStatus() );
+
+        return planning1.build();
+    }
+}
