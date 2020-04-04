51432930babc91122851a0aaf43e42950ba9eba1
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-04-04 13:10:08 +0200

    Custom Mapping for projection

--- ospp-application/src/main/java/ch/fhnw/ip6/ospp/vo/LecturerVO.java
+++ ospp-application/src/main/java/ch/fhnw/ip6/ospp/vo/LecturerVO.java
@@ -1,9 +1,13 @@
 package ch.fhnw.ip6.ospp.vo;
 
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface LecturerVO {
 
+    @Value("#{target.externalId}")
     int getId();
+
     int getExternalId();
 
     String getFirstname();
