1fdeb3e5ff8f8cb41ca67d15718759ae6d482eb0
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-04-05 11:46:21 +0200

    fixed constraint violation

--- ospp-application/src/main/java/ch/fhnw/ip6/ospp/model/Planning.java
+++ ospp-application/src/main/java/ch/fhnw/ip6/ospp/model/Planning.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import java.sql.Clob;
@@ -23,6 +25,7 @@ import java.time.LocalDateTime;
 public class Planning {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String nr;
