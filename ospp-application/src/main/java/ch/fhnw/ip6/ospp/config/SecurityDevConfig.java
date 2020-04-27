ac8568c7aedaee26c6cf374769604fc84148e41c
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-04-27 10:06:18 +0200

    disable security on dev

--- ospp-application/src/main/java/ch/fhnw/ip6/ospp/config/SecurityDevConfig.java
+++ ospp-application/src/main/java/ch/fhnw/ip6/ospp/config/SecurityDevConfig.java
@@ -1,8 +1,6 @@
 package ch.fhnw.ip6.ospp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,23 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Profile("!dev")
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class SecurityDevConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeRequests().anyRequest().permitAll()
                 .and()
                 .httpBasic();
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("sieglinde.schreiner@edubs.ch")
-                .password("{noop}fms2020")
-                .roles("USER");
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        // do nothing
     }
 }
