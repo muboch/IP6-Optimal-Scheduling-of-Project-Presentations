b18ef330edbd018c3d0e96a1062470f252c39485
Peer Jüttner <peer.juettner@sitewerk.ch>
2020-05-11 15:23:14 +0200

    sidebar and planningservice creation

---
+++ frontend/src/Services/planningService.ts
@@ -0,0 +1,48 @@
+import { APIROUTES } from "../constants";
+import { Lecturer } from "../Types/types";
+
+export const loadPlannings = async (): Promise<Array<Lecturer>> => {
+  try {
+    const res = await fetch(`${APIROUTES.planning}`);
+    const json = await res.json();
+    console.log(json);
+    if (res.ok) {
+      return json;
+    }
+    throw "Fehler beim laden der Dozenten";
+  } catch (Error) {
+    throw Error;
+  }
+};
+
+export const getPlanningById = async (id: number): Promise<Lecturer> => {
+  try {
+    const res = await fetch(`${APIROUTES.planning}/${id}`);
+    const json = await res.json();
+    console.log(json);
+    if (res.ok) {
+      return json;
+    }
+    throw "Fehler beim laden des Dozenten";
+  } catch (Error) {
+    throw Error;
+  }
+};
+
+
+export const deletePlanningById = async (id: number): Promise<void> => {
+  // Default options are marked with *
+  const response = await fetch(`${APIROUTES.planning}/${id}`, {
+    method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+};
