8a6a824923a37026fb86f156b0a42d0e9182cddf
Peer Jüttner <peer.juettner@sitewerk.ch>
2020-05-15 11:34:31 +0200

    logic to check initials

--- frontend/src/Services/lecturerService.ts
+++ frontend/src/Services/lecturerService.ts
@@ -54,17 +54,24 @@ export const addLecturer = async (
 
 export const deleteLecturerById = async (id: number): Promise<void> => {
   // Default options are marked with *
-  const response = await fetch(`${APIROUTES.lecturer}/${id}`, {
-    method: "DELETE", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
+  try {
+    const res = await fetch(`${APIROUTES.lecturer}/${id}`, {
+      method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    if (res.ok) {
+      return;
+    }
+    throw "Fehler beim laden des Dozenten";
+  } catch (Error) {
+    throw Error;
+  }
 };
