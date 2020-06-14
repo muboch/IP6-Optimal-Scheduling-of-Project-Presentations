59ce3c00f3f1398b19f6e7ca5dfa7e305184a2b9
Peer Jüttner <peer.juettner@sitewerk.ch>
2020-06-14 11:51:20 +0200

    fix opta

--- solvers/optasolver/src/main/java/ch/fhnw/ip6/optasolver/constraints/varlistener/FreeTimeslotsUpdatingVarListener.java
+++ solvers/optasolver/src/main/java/ch/fhnw/ip6/optasolver/constraints/varlistener/FreeTimeslotsUpdatingVarListener.java
@@ -5,43 +5,62 @@ import ch.fhnw.ip6.optasolver.model.Presentation;
 import org.optaplanner.core.impl.domain.variable.listener.VariableListener;
 import org.optaplanner.core.impl.score.director.ScoreDirector;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 
-public class FreeTimeslotsUpdatingVarListener implements VariableListener<Lecturer> {
+public class FreeTimeslotsUpdatingVarListener implements VariableListener<Presentation> {
 
     @Override
-    public void beforeEntityAdded(ScoreDirector scoreDirector, Lecturer lecturer) {
+    public void beforeEntityAdded(ScoreDirector scoreDirector, Presentation presentation) {
 
     }
 
     @Override
-    public void afterEntityAdded(ScoreDirector scoreDirector, Lecturer lecturer) {
+    public void afterEntityAdded(ScoreDirector scoreDirector, Presentation presentation) {
 
     }
 
     @Override
-    public void beforeVariableChanged(ScoreDirector scoreDirector, Lecturer lecturer) {
+    public void beforeVariableChanged(ScoreDirector scoreDirector, Presentation presentation) {
 
     }
 
     @Override
-    public void afterVariableChanged(ScoreDirector scoreDirector, Lecturer lecturer) {
-
-        Presentation firstPres = lecturer.getPresentations().stream().min(Comparator.comparingInt(p -> p.getTimeslot().getId())).get(); // change timeslot.getId to timeslot.getOrder
-        Presentation lastPres = lecturer.getPresentations().stream().max(Comparator.comparingInt(p -> p.getTimeslot().getId())).get(); // change timeslot.getId to timeslot.getOrder
-        int numPres = lecturer.getPresentations().size();
-        // Number of Free timeslots is = numPres - (lastPres - firstPres);
-        lecturer.setFreeTimeslots(numPres - (lastPres.getTimeslot().getId() - firstPres.getTimeslot().getId()));
+    public void afterVariableChanged(ScoreDirector scoreDirector, Presentation presentation) {
+updateFreeTimeslots(scoreDirector,presentation);
     }
 
     @Override
-    public void beforeEntityRemoved(ScoreDirector scoreDirector, Lecturer lecturer) {
+    public void beforeEntityRemoved(ScoreDirector scoreDirector, Presentation presentation) {
 
     }
 
     @Override
-    public void afterEntityRemoved(ScoreDirector scoreDirector, Lecturer lecturer) {
+    public void afterEntityRemoved(ScoreDirector scoreDirector, Presentation presentation) {
 
     }
+    private void updateFreeTimeslots(ScoreDirector scoreDirector, Presentation presentation) {
+        List<Lecturer> coachAndExpert = new ArrayList<>();
+        coachAndExpert.add((Lecturer) presentation.getCoach());
+        coachAndExpert.add((Lecturer) presentation.getExpert());
+
+        coachAndExpert.forEach(l -> {
+            if (l.getPresentations() == null){return;}
+            if (l.getPresentations().stream().map(p -> p.getTimeslot()).filter(t -> t == null).count() > 0){
+                return;
+            }
+            Presentation firstPres = l.getPresentations().stream().min(Comparator.comparingInt(p -> p.getTimeslot().getId())).get(); // change timeslot.getId to timeslot.getOrder
+            Presentation lastPres = l.getPresentations().stream().max(Comparator.comparingInt(p -> p.getTimeslot().getId())).get(); // change timeslot.getId to timeslot.getOrder
+            Integer numPres = l.getPresentations().size();
+            Integer freeTimeslots = (numPres - (numPres - (lastPres.getTimeslot().getId() - firstPres.getTimeslot().getId())));
+
+            scoreDirector.beforeVariableChanged(l, "freeTimeslots");
+            l.setFreeTimeslots(freeTimeslots);
+            scoreDirector.afterVariableChanged(l, "freeTimeslots");
+        });
+    }
+
+
 }
