37944a32f1554d6d7fd93d674fa570762704e403
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-06-15 14:48:43 +0200

    Update MeasuringController.java

--- ospp-application/src/main/java/ch/fhnw/ip6/ospp/controller/MeasuringController.java
+++ ospp-application/src/main/java/ch/fhnw/ip6/ospp/controller/MeasuringController.java
@@ -6,11 +6,17 @@ import ch.fhnw.ip6.ospp.service.FachlicheException;
 import ch.fhnw.ip6.ospp.service.PlanningService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -20,6 +26,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MeasuringController {
 
     private final PlanningService planningService;
+
+    @GetMapping
+    public ResponseEntity<String[]> measuring() {
+        String[] endpoints = {"ortools", "ilp", "opta"};
+        return ResponseEntity.ok(Arrays.stream(endpoints).map(e -> "/api/measuring/" + e).toArray(String[]::new));
+    }
 
 
     @GetMapping("ortools")
