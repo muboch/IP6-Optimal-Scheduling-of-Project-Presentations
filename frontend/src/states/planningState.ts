26eaa1d65e13090b46b318cba1d76c48f15c2c5a
Peer Jüttner <peer.juettner@sitewerk.ch>
2020-06-15 13:28:12 +0200

    add planningstate to frontend

---
+++ frontend/src/states/planningState.ts
@@ -0,0 +1,27 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { createContainer } from "unstated-next";
+import { useState, useEffect } from "react";
+import { getIsSolving } from "../Services/planningService";
+
+const planningState = () => {
+  const [isSolving, setIsSolving] = useState<boolean>(false);
+
+  useEffect(() => {
+    const load = async () => {
+      console.log("getPlanning");
+
+      const is = await getIsSolving();
+      setIsSolving(is);
+    };
+    const interval = setInterval(load, 2500);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return {
+    isSolving,
+  };
+};
+const PlanningContainer = createContainer(planningState);
+export default PlanningContainer;
