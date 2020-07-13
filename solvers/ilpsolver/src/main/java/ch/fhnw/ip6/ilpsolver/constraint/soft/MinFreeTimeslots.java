6fa94eeddb51e0619f1d918b187834ac1e8965c2
Carlo Eggenschwiler <carlo.eggenschwiler@students.fhnw.ch>
2020-07-13 17:00:42 +0200

    Revert "Update MinFreeTimeslots.java"
    This reverts commit 117871a1f49268b214297d05eb852a56bc51e052.

--- solvers/ilpsolver/src/main/java/ch/fhnw/ip6/ilpsolver/constraint/soft/MinFreeTimeslots.java
+++ solvers/ilpsolver/src/main/java/ch/fhnw/ip6/ilpsolver/constraint/soft/MinFreeTimeslots.java
@@ -55,16 +55,14 @@ public class MinFreeTimeslots extends SoftConstraint {
                         usedExpr.addTerm(1.0, lecTime[indexOf(l)][indexOf(tBeg)]);
                         usedExpr.addTerm(1.0, lecTime[indexOf(l)][indexOf(tEnd)]);
                         usedExpr.addConstant(-1);
-
-                        getGrbModel().addGenConstrIndicator(used, 1, usedExpr, GRB.GREATER_EQUAL,1.0, null);
-                        getGrbModel().addGenConstrIndicator(used, 0, usedExpr, GRB.LESS_EQUAL,0.0, null);
+                        getGrbModel().addConstr(used, GRB.GREATER_EQUAL, usedExpr, null);
 
                         int abs = tEnd.getSortOrder() - tBeg.getSortOrder() - getIlpModel().getPresentationsPerLecturer().get(l).size() + 1;
 
                         GRBLinExpr rhs = new GRBLinExpr();
                         rhs.addTerm(abs, used);
-                        getGrbModel().addGenConstrIndicator(diff, 1, rhs, GRB.GREATER_EQUAL,1.0, null);
-                        getGrbModel().addGenConstrIndicator(diff, 0, rhs, GRB.LESS_EQUAL,0.0, null);
+
+                        getGrbModel().addConstr(diff, GRB.GREATER_EQUAL, rhs, null);
 
                     }
                 }
